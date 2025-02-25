d07e1d84fb87f87c32645f8331f556a3751f712a
Victor Baptista do Rego <victor@Victors-MacBook-Pro.local>
2025-02-25 20:53:30 +0000

    Database production enviroment

--- config/config.js
+++ config/config.js
@@ -13,6 +13,10 @@ module.exports = {
     dialect: 'postgres',
     dialectOptions: { ssl: { require: true } },
     */
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.SQL,
+    host: process.env.PGHOST,
     url: process.env.SQL,
     dialect: 'postgres',
     dialectOptions: { ssl: { require: true } },
